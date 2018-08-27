54de22a0bc23a19a75d1b62e90107a67fecb8c36
Franco <franco.soto.z@gmail.com>
2018-08-27 01:27:09 -0300

    Establecer servidor seguro

--- app.js
+++ app.js
@@ -4,19 +4,25 @@
  * resourcePath: /api
  * description: All about API
  */
-
+const dns = require('dns');
 const _ = require('underscore'); //Sólamente para tener algunas herramientas más para desarrollar
 const restify = require('restify'); //framework REST
+const fs = require('fs'); //filesystem
 const corsMiddleware = require('restify-cors-middleware')
 // var debug = require('debug');
-const sequelize = new (require('sequelize'))('sgpp', 'root', 'root', {host: 'localhost',dialect: 'mysql'});
+// const sequelize = new (require('sequelize'))('sgpp', 'root', 'root', {host: 'localhost',dialect: 'mysql'});
 // const sequelize = new Sequelize('sgpp', 'root', 'root', {host: 'localhost',dialect: 'mysql'});
 const cors = corsMiddleware({
 	origins: ['*'],
 	allowHeaders: ['Authorization']
 })
 
-const app = restify.createServer({name: 'sgppApi'});
+const app = restify.createServer({
+	name: 'sgppApi',
+	certificate: fs.readFileSync('/var/certs/ssl/cert.pem'),
+	// chain: fs.readFileSync('/var/certs/ssl/chain.pem'),
+	key: fs.readFileSync('/var/certs/ssl/privkey.pem')
+});
 
 app.pre((req,res,next) => {
 	console.log('Request URL: '+req.method+' '+req.url);
@@ -38,7 +44,11 @@ for(var x = 0; routesArr.length > x; x++) {
 	require("./routes/" + routesArr[x])(app);
 }
 
-app.listen(3000,'0.0.0.0', () => {
+dns.lookup('api.forkb.com.ar', function (err, addresses, family) {
+  console.log(addresses);
+});
+
+app.listen(3000, '0.0.0.0', () => {
   	console.log('Server "%s" escuchando a la URI %s',app.name,app.url);
   	// debug('Server escuchando el puerto 3000 al server %s',app.url)
 });
