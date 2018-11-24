4ddf5655186b5aa8e3159ad09e9735d8eb96d2d2
Franco <franco.soto.z@gmail.com>
2018-11-24 15:22:10 -0300

    Config deploy

--- app.js
+++ app.js
@@ -9,21 +9,29 @@ const restify = require('restify'); //framework REST
 const fs = require('fs'); //filesystem
 const corsMiddleware = require('restify-cors-middleware')
 // var debug = require('debug');
-// const sequelize = new (require('sequelize'))('sgpp', 'root', 'root', {host: 'localhost',dialect: 'mysql'});
-// const sequelize = new Sequelize('sgpp', 'root', 'root', {host: 'localhost',dialect: 'mysql'});
+
 const cors = corsMiddleware({
 	origins: ['*'],
 	allowHeaders: ['Authorization']
 })
 
-const app = restify.createServer({
+var config = {
 	name: 'sgppApi',
-	// certificate: fs.readFileSync('/var/certs/ssl/cert.pem'),
-	// key: fs.readFileSync('/var/certs/ssl/privkey.pem')
+}
+
+if (process.env.NODE_ENV === 'production') {
+	config = _.extend(config, {
+		certificate: fs.readFileSync(process.env.CERT_SSL),
+		key: fs.readFileSync(process.env.PKEY_SSL)
+	})
+}
+
+const app = restify.createServer({
+	name: 'sgppApi'
 });
 
 app.pre((req,res,next) => {
-	console.log('Request URL: '+req.method+' '+req.url);
+	console.log('Request URL: ' + req.method + ' ' + req.url);
 	next()
 });
 
@@ -36,13 +44,16 @@ app.get('/status',(req,res,next)=>{
 	res.send('ready')
 });
 
-var routesArr = ["auth","porpose","node","cicle","stage","person","user","action","implementation","rol","reports","task","labels"]
+var routesArr = [
+	"auth","porpose","node","cicle","stage","person","user","action","implementation","rol",
+	"reports","task","labels"
+]
 
 for(var x = 0; routesArr.length > x; x++) {
 	require("./routes/" + routesArr[x])(app);
 }
 
 app.listen(3000, '0.0.0.0', () => {
-  	console.log('Server "%s" escuchando a la URI %s',app.name,app.url);
+  	console.log('Server "%s" escuchando a la URI %s', app.name, app.url);
   	// debug('Server escuchando el puerto 3000 al server %s',app.url)
 });
